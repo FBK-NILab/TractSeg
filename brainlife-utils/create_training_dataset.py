a8d70ebf36070f6e2e86a45f77fe8e1361adee71
Giulia Bertò <giulia.berto.4@gmail.com>
2019-07-11 15:12:05 +0200

    Update create_training_dataset.py

--- brainlife-utils/create_training_dataset.py
+++ brainlife-utils/create_training_dataset.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from data_utils import create_dataset
+from data_utils import create_training_dataset
 
 if __name__ == '__main__':
     # create dataset for training
     print('starting dataset creation')
-    create_dataset('config.json')
+    create_training_dataset('config.json')
     print('dataset for training created')
 
